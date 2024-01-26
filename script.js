6a64e1bc932f340119df8a3eca35bb60c09698a6
Alf <alfgoto@gmail.com>
2024-01-26 22:29:17 +0100

    nice start of an UI

--- script.js
+++ script.js
@@ -21,30 +21,28 @@ dc(() => {
 
 
 
-
-
-
     let copy = gid('copy')
     let download = gid('download')
     let downloadA = gid('downloadA')
     cl(copy, () => {
         navigator.clipboard.writeText("<script src='https://alframework.netlify.app/alframework.js'></script>");
-        alert('link copied')
+        addAnimateButton(copy)
     })
     cl(download, () => {
         downloadA.click()
+        addAnimateButton(download)
     })
-
 
     let alfjaxcopy = gid('alfjaxcopy')
     let alfjaxdownload = gid('alfjaxdownload')
     let alfjaxdownloadA = gid('alfjaxdownloadA')
     cl(alfjaxcopy, () => {
         navigator.clipboard.writeText("<script src='https://alframework.netlify.app/AlfJAX.js'></script>");
-        alert('link copied')
+        addAnimateButton(alfjaxcopy)
     })
     cl(alfjaxdownload, () => {
         alfjaxdownloadA.click()
+        addAnimateButton(alfjaxdownload)
     })
 
     let Shortercopy = gid('Shortercopy')
@@ -52,14 +50,20 @@ dc(() => {
     let downloadSHORTER = gid('downloadSHORTER')
     cl(Shortercopy, () => {
         navigator.clipboard.writeText("<script src='https://alframework.netlify.app/Shorter.js'></script>");
-        alert('link copied')
+        addAnimateButton(Shortercopy)
     })
     cl(Shorterdownload, () => {
         downloadSHORTER.click()
+        addAnimateButton(Shorterdownload)
     })
 
 
 
+
+    function addAnimateButton(arg){
+        arg.classList.add('buttonANIMATION')
+        setTimeout(()=>{arg.classList.remove('buttonANIMATION')},1500)
+    }
 
     alframeworkhelp()
 })
